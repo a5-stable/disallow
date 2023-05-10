d24ae6e041991aaf19ee28d7cf79eb92e49f1ac6
a5-stable <sh07e1916@gmail.com>
2023-05-10 19:28:03 +0900

    initial commit

---
+++ lib/disallow.rb
@@ -0,0 +1,6 @@
+require "disallow/version"
+
+module Disallow
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/disallow_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Disallow do
+  it "has a version number" do
+    expect(Disallow::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
